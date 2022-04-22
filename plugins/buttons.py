3878fd192bf9caa9469a4382611127e42c9933fc
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-22 14:38:28 +0530

    Create buttons.py

---
+++ plugins/buttons.py
@@ -0,0 +1,23 @@
+from pyrogram.types import InlineKeyboardButton
+
+KGF_D_BUTTON = [[
+               InlineKeyboardButton("KGF CHAPTER 1", callback_data="downlod"),
+               InlineKeyboardButton("KGF CHAPTER 2", callback_data="smovies")
+               ],[
+               InlineKeyboardButton("📙 1/1", "dupe"),
+               InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")            
+               ]]
+
+KGF_BUTTON = [[
+             InlineKeyboardButton("♻️ KGF : CHAPTER 1 ♻️", "dupe")            
+             ],[
+             InlineKeyboardButton("മലയാളം", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOAYAAnHRAVflSU0XBg9YoRYE"),
+             InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE")            
+             ],[
+             InlineKeyboardButton("HINDI", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPQYAAnHRAVergKA1x56J1hYE"),
+             InlineKeyboardButton("TELUGU", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOwYAAnHRAVc8R-uNd43pLBYE"),
+             InlineKeyboardButton("KANNADA", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPAYAAnHRAVd6dci1UtsI9BYE")
+             ],[
+             InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="kgfdownlod"),
+             InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
+             ]]
