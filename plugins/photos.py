32b7681e59c7289b797075f4fd12da60477a475a
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-22 14:41:39 +0530

    Create photos.py

---
+++ plugins/photos.py
@@ -0,0 +1,6 @@
+
+KGF_PHOTOS = [
+             "https://telegra.ph/file/1a39311dbf13ca400ee5a.jpg",
+             "https://telegra.ph/file/f83a205dcfd1cdc33baab.jpg",
+             "https://telegra.ph/file/8712112da8ba9f593e604.jpg",            
+             ]
