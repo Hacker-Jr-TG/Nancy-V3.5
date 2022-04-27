b593e1bd2b594a70383515639bb46e816ec4eb68
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-27 14:56:54 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -21,3 +21,17 @@ KGF_BUTTON = [[
              InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="kgfdownlod"),
              InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
              ]]
+
+HEIST_BUTTON = [[
+             InlineKeyboardButton("🎭 MONEY HEIST SERIES 🎭", "dupe")            
+             ],[
+             InlineKeyboardButton("Season 1", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE"),
+             InlineKeyboardButton("Season 2", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE")            
+             ],[
+             InlineKeyboardButton("Season 3", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE"),
+             InlineKeyboardButton("Season 4", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE"),
+             InlineKeyboardButton("Season 5", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADjAUAAr4wSVd5ZIiqNPd8WhYE")
+             ],[
+             InlineKeyboardButton("📗 1/1", "dupe"),
+             InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
+             ]]
