cfe95d68bf35ade3fe9431690dac18761af8d340
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-27 15:06:40 +0530

    Update photos.py

--- plugins/photos.py
+++ plugins/photos.py
@@ -4,3 +4,8 @@ KGF_PHOTOS = [
              "https://telegra.ph/file/f83a205dcfd1cdc33baab.jpg",
              "https://telegra.ph/file/8712112da8ba9f593e604.jpg",            
              ]
+
+HEIST_PHOTOS = [
+             "https://telegra.ph/file/fa9d59bbcab85a0c080cc.jpg"
+             "https://telegra.ph/file/18a29179a5b75cb3d3d93.jpg",            
+             ]
