0b8cec81d2a695c10bb615079c3834cbe90e29b1
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-05-06 16:10:40 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -9,7 +9,7 @@ KGF_D_BUTTON = [[
                ]]
 
 KGF_BUTTON = [[
-             InlineKeyboardButton("♻️ KGF : CHAPTER 1 ♻️", "dupe")            
+             InlineKeyboardButton("♻️ KGF : CHAPTER 1 HDRip ♻️", "dupe")            
              ],[
              InlineKeyboardButton("മലയാളം", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOAYAAnHRAVflSU0XBg9YoRYE"),
              InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE")            
@@ -17,6 +17,17 @@ KGF_BUTTON = [[
              InlineKeyboardButton("HINDI", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPQYAAnHRAVergKA1x56J1hYE"),
              InlineKeyboardButton("TELUGU", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOwYAAnHRAVc8R-uNd43pLBYE"),
              InlineKeyboardButton("KANNADA", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPAYAAnHRAVd6dci1UtsI9BYE")
+             ],[
+             InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="kgfdownlod"),
+             InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
+             ]]
+
+KGF2_BUTTON = [[
+             InlineKeyboardButton("⚜️ KGF : CHAPTER 2 HDRip ⚜️", "dupe")            
+             ],[
+             InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE")            
+             ],[
+             InlineKeyboardButton("❗️ Tamil Dubbed Only Available ❗️", "dupe")
              ],[
              InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="kgfdownlod"),
              InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
